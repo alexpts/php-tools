1ea96ef23ae2a6436ab33d140a28fa8645e0ce45
alexpts <alexpts@yandex.ru>
2016-09-18 10:38:35 +0300

    Chain of Responsibility to interface signature

--- src/PTS/Tools/CollectionInterface.php
+++ src/PTS/Tools/CollectionInterface.php
@@ -4,13 +4,27 @@ namespace PTS\Tools;
 
 interface CollectionInterface
 {
+    /**
+     * @param string $name
+     * @param mixed $item
+     * @param int $priority
+     * @return $this
+     */
     public function addItem(string $name, $item, int $priority = 50);
 
+    /**
+     * @param string $name
+     * @param int $priority
+     * @return $this
+     */
     public function removeItem(string $name, int $priority = null);
 
     public function has(string $name) : bool;
 
     public function getItems() : array;
 
+    /**
+     * @return $this
+     */
     public function flush();
 }
